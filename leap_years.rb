64bb00614864211edd7d4960932c66c833b00471
Matt Rose <yamiacat@gmail.com>
2017-03-30 12:56:56 +0100

    Add leap_years, first attempt, doesn't work

---
+++ leap_years.rb
@@ -0,0 +1,21 @@
+puts "I can tell you which years are leap years!"
+puts "Give me a starting year: "
+s_year = gets.chomp.to_i
+puts "Give me an end year: "
+e_year = gets.chomp.to_i
+
+puts "The leap years between #{s_year} and #{e_year} are:"
+
+year = s_year
+
+while year <= e_year
+  if year % 4 != 0
+    year += 1 
+  elsif (year % 100 == 0) && (year % 400 != 0)
+    year += 1
+  elsif 
+    print "#{year}, " 
+    year += 1
+  end
+
+end
